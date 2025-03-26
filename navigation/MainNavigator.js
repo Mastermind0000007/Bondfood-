50fa3c6815a089da9da7c891c4c40c77b8ffb284
Mastermind0000007 <talktomude@gmail.com>
2025-03-26 02:10:31 +0000

    Create MainNavigator.js

---
+++ navigation/MainNavigator.js
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { AuthContext } from '../context/AuthContext';
+import HomeScreen from '../screens/HomeScreen';
+import LoginScreen from '../screens/LoginScreen';
+import SignupScreen from '../screens/SignupScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+
+const Stack = createStackNavigator();
+
+export default function MainNavigator() {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <Stack.Navigator>
+      {user ? (
+        <>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Profile" component={ProfileScreen} />
+        </>
+      ) : (
+        <>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Signup" component={SignupScreen} />
+        </>
+      )}
+    </Stack.Navigator>
+  );
+        }
